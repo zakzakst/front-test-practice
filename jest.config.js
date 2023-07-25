f77b0e3e674b709c4986f71b3ca72afbaa8fb278
zakzakst <e1433104@gmail.com>
2023-07-25 21:50:59 +0900

    5-3

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: { '^.+\\.(ts|tsx)$': ['esbuild-jest', { sourcemap: true }] },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 };
