4be58cd098410e7dea3f01a0e69f12fd92eef92e
zakzakst <e1433104@gmail.com>
2023-07-28 22:47:08 +0900

    5-5

---
+++ src/05/05/InputAccount.tsx
@@ -0,0 +1,18 @@
+export const InputAccount = () => {
+  return (
+    <fieldset>
+      <legend>アカウント情報の入力</legend>
+      <div>
+        <label>
+          メールアドレス
+          <input type="text" placeholder="example@test.com" />
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="password" placeholder="8文字以上で入力" />
+        </label>
+      </div>
+    </fieldset>
+  )
+}
