4139d2b406038df497831c72b62964167defca37
zakzakst <e1433104@gmail.com>
2023-08-05 20:17:38 +0900

    06

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
   testEnvironment: 'jest-environment-jsdom',
   transform: { '^.+\\.(ts|tsx)$': ['esbuild-jest', { sourcemap: true }] },
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '__reports__',
+        filename: 'jest.html',
+      },
+    ],
+  ],
 };
